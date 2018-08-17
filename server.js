4517d01f3c1f2156ca6ec386b4a90317821abd74
Carlos Casares <ccasares@gmail.com>
2018-08-17 18:16:48 +0200

    new

---
+++ server.js
@@ -0,0 +1,70 @@
+'use strict';
+
+// Module imports
+const express = require('express')
+    , http = require('http')
+    , bodyParser = require('body-parser')
+    , log = require('npmlog-ts')
+    , _ = require('lodash')
+    , RaspiCam = require("raspicam")
+    , Quagga = require('quagga').default
+    , uuid = require('uuid/v4')
+;
+
+log.level = 'verbose';
+log.timestamp = true;
+
+// Initializing REST server BEGIN
+const PORT = process.env.READERPORT || 8886
+    , restURI    = '/reader'
+    , pictureURI = '/take'
+    , lastURI    = '/last'
+;
+
+var app    = express()
+  , router = express.Router()
+  , server = http.createServer(app)
+;
+
+// Misc
+const PROCESS = 'PROCESS'
+    , REST    = 'REST'
+;
+
+// Detect CTRL-C
+process.on('SIGINT', function() {
+  log.info(PROCESS, "Caught interrupt signal");
+  log.info(PROCESS, "Exiting gracefully");
+  if (board) board.close()
+  board = undefined;
+  process.removeAllListeners()
+  if (typeof err != 'undefined')
+    log.error(PROCESS, err)
+  process.exit(2);
+});
+
+var camera = new RaspiCam({
+    mode: "photo",
+    encoding: "jpg",
+    timeout: 500 // take the picture immediately
+});
+
+camera.on("start", (err, timestamp) => {
+    console.log(" photo started at " + timestamp );
+});
+
+camera.on("read", (err, timestamp, filename) => {
+    console.log(" read at " + timestamp + " with filename "+ filename);
+});
+
+camera.on("exit", (timestamp) => {
+    console.log(" exit at " + timestamp );
+});
+
+router.get(restURI, (req, res) => {
+  var filename = uuid();
+  camera.set("output", "./images/" + filename);
+  camera.start();
+  res.status(204).send();
+  res.end();
+});
